f65f30219adb944ea44e26e8c1abfeeaa0a1ae71
Renupriyaaa <renupriyamannem@gmail.com>
2026-02-28 21:21:04 +0530

    msg

--- IV-SEM/Python/Practice/M02_Logic_Building_and_patterns/S06/PS07_Advanced_patterns.py
+++ IV-SEM/Python/Practice/M02_Logic_Building_and_patterns/S06/PS07_Advanced_patterns.py
@@ -7,4 +7,32 @@
                         1 3 3 1
                         1 4 6 4 1
 
+   n = int(input("Enter the number of rows: "))
+   for i in range(n):
+       num = 1
+       for j in range(i + 1):
+           print(num, end=" ")
+           num = num * (i - j) // (j + 1)
+       print()                   
  """
+
+# Butterfly Pattern
+ """
+ -->Sample Input : 5
+    Sample Output :     *       *
+                        **     **
+                        ***   ***
+                        **** ****
+                        *********
+                        **** ****
+                        ***   ***
+                        **     **
+                        *       *
+ 
+n = int(input("Enter the number of rows: "))
+for i in range(1, n + 1):
+    print("*" * i + " " * (2 * (n - i)) + "*" * i)
+
+for i in range(n - 1, 0, -1):
+    print("*" * i + " " * (2 * (n - i)) + "*" * i)
+"""
